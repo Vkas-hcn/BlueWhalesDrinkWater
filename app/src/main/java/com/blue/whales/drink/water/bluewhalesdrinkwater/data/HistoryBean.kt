a1b2d323e9973a501856d4ad0ec5dee380f9cd4c
vkas <vkas@qq.com>
2025-04-28 18:33:53 +0800

    feat: hunxiao

--- app/src/main/java/com/blue/whales/drink/water/bluewhalesdrinkwater/data/HistoryBean.kt
+++ app/src/main/java/com/blue/whales/drink/water/bluewhalesdrinkwater/data/HistoryBean.kt
@@ -1,13 +1,9 @@
 package com.blue.whales.drink.water.bluewhalesdrinkwater.data
 
+import androidx.annotation.Keep
 
-/**
- * 历史记录数据类
- * @param date 日期（格式为 "yyyy-MM-dd"）
- * @param totalDrink 当天总喝水量（单位：ml）
- * @param cupCount 当天喝水杯数
- * @param progress 当天喝水目标进度值（百分比）
- */
+
+@Keep
 data class HistoryBean(
     val date: String,
     val totalDrink: Int,
--- app/src/main/java/com/blue/whales/drink/water/bluewhalesdrinkwater/data/WaterBean.kt
+++ app/src/main/java/com/blue/whales/drink/water/bluewhalesdrinkwater/data/WaterBean.kt
@@ -1,9 +1,12 @@
 package com.blue.whales.drink.water.bluewhalesdrinkwater.data
 
+import androidx.annotation.Keep
+
+@Keep
 data class WaterBean (
     var id: String,
     var date: String,
     var goalNum: Int,
     var drinkNum: Int,
-    var drinkTime: String // 新增字段，表示喝水时间，格式为 "HH:mm"
+    var drinkTime: String
 )
